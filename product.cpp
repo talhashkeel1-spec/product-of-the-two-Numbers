8788468338c6e5f84c95d79cb7a795c347d836d0
Muhammad Talha <talhashkeel1@gmail.com>
2026-04-14 23:59:10 +0500

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+int num1,num2,product;
+cout<< " enter two numbers: ";
+cin>>num1>>num2;
+product=num1* num2 ;
+cout <<"Product="<<product;
+return 0 ;
+}
